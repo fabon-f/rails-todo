aff87291e6416723cd32351fe9fe5bf435391525
hinamiyagk <hinamiy@npca-1.info>
2015-06-09 23:41:20 +0900

    Configure autoload of app/decorators

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,8 +1,8 @@
 # coding: utf-8
 module UserDecorator
   def gravatar
-    gravatar_id = Digest::MD5::hexdigest(self.email.downcase)
+    gravatar_id = Digest::MD5::hexdigest(email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: self.username)
+    image_tag(gravatar_url, alt: username)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module ToDo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.autoload_paths << Rails.root.join('app/decorators')
     config.generators do |g|
       g.helper false
       g.test_framework :rspec, view_specs: false, helper_specs: false, controller_specs: false, fixture: true
